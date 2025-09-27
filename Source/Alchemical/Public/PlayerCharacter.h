0b89aa709f75b80db759fd33d9a85b5773dda2bd
Yousef AlShaikh <164582835+yousefalshaikh17@users.noreply.github.com>
2025-09-27 21:55:03 +0100

    refactor: Create Player Character C++ class
    - Initial commit for player character conversion to C++
    - Update references in other objects

---
+++ Source/Alchemical/Public/PlayerCharacter.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CarriedItem.h"
+#include "ElementTypesDisplayWidget.h"
+#include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "PaperSpriteComponent.h"
+#include "PlantData.h"
+#include "PlayerCharacter.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnHeldItemChanged, APlayerCharacter*, Character, ECarriedItem, CarriedItemType, int, PlantIndex);
+
+UCLASS()
+class ALCHEMICAL_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USceneComponent* CameraTransform;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveInputAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UElementTypesDisplayWidget* ElementsDisplayWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPaperSpriteComponent* PlantSpriteComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPaperSprite* SeedSprite;
+
+	
+
+	// Called when the game starts or when spawned
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void Move(float RightScale, float ForwardScale);
+
+    UFUNCTION(BlueprintNativeEvent)
+	void HandleMoveInput(const FInputActionValue& Value);
+	
+	bool GetHeldPlantData(FPlantData& PlantData) const;
+	void UpdateHeldItemDisplay() const;
+
+	bool IsSameCarriedItem(ECarriedItem NewCarriedItemType, int NewPlantIndex) const;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bMovementEnabled = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ECarriedItem CarriedItemType = ECarriedItem::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int PlantIndex = -1;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void UpdateHeldItem(ECarriedItem NewCarriedItemType, int NewPlantIndex);
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHeldItemChanged OnHeldItemChanged;
+
+};
