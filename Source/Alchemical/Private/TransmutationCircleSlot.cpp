72cf5058472828b456284d16791bef61ca0e8176
Yousef AlShaikh <164582835+yousefalshaikh17@users.noreply.github.com>
2025-09-27 04:02:58 +0100

    refactor: ItemSlotActor as superclass for slots
    - ItemSlotActor created to represent actors that can hold an item.
    - Update Transmutation Circle and Ingredient Slots to be subclassed to this.

--- Source/Alchemical/Private/TransmutationCircleSlot.cpp
+++ Source/Alchemical/Private/TransmutationCircleSlot.cpp
@@ -10,29 +10,3 @@ ATransmutationCircleSlot::ATransmutationCircleSlot()
 	PrimaryActorTick.bCanEverTick = false;
 
 }
-
-void ATransmutationCircleSlot::PlaceIngredient_Implementation(int32 NewPlantIndex)
-{
-	if (NewPlantIndex < 0)
-	{
-		UE_LOG(LogTemp, Error, TEXT("Plant index cannot be negative."));
-		return;
-	}
-
-	this->PlantIndex = NewPlantIndex;
-}
-
-void ATransmutationCircleSlot::ClearIngredient_Implementation()
-{
-	this->PlantIndex = -1;
-}
-
-int ATransmutationCircleSlot::GetCurrentIngredient() const
-{
-	return PlantIndex;
-}
-
-bool ATransmutationCircleSlot::IsOccupied() const
-{
-	return PlantIndex > -1;
-}
